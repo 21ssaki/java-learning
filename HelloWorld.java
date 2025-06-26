113cbf8485e5de595e718d6e8d0de603d7779060
saki <gissaki21@gmail.com>
2025-06-27 07:52:36 +0900

    Update HelloWorld.java
    HelloWorldプログラム作成、変数追加

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,13 @@
+public class HelloWorld{
+  public static void main(String[] args){
+                          //最初のJavaプログラム
+                          System.out.println("Hello,World!");
+                          System.out.println("Java学習を開始しました!");
+                          //変数の練習
+                          String name="Java初心者";
+                          int age = 25;
 
+                          System.out.println("私の名前は"+ name +"です");
+                          System.out.println("年齢は"+ age +"歳です");
+  }
+}
